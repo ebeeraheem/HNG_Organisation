8023d7fa699919663c21e9dc559cde4ff8b98d52
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-07 18:22:54 +0100

    Add new entity: Organisation

---
+++ HNG_Organisation/Entities/Organisation.cs
@@ -0,0 +1,8 @@
+﻿namespace HNG_Organisation.Entities;
+
+public class Organisation
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
