9ca3f3007453e4775cdb282e170d6a3159131595
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-07 15:24:47 +0100

    Add login and register models

---
+++ HNG_Organisation/Models/RegisterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HNG_Organisation.Models;
+
+public class RegisterModel
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
