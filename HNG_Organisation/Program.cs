cebe6f5fe08a895e572b99e1ac102ebe682022f2
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-07 15:06:33 +0100

    Configure connection strings

--- HNG_Organisation/Program.cs
+++ HNG_Organisation/Program.cs
@@ -1,9 +1,14 @@
+using HNG_Organisation.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
