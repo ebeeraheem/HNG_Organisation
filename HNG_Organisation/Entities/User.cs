aceffef44d7766fd3f8d3829495bf0faccef5cda
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-07 15:15:07 +0100

    Add user entity

---
+++ HNG_Organisation/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HNG_Organisation.Entities;
+
+public class User : IdentityUser
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+
+    // The userId, email, password and phone properties are inherited from IdentityUser
+}
