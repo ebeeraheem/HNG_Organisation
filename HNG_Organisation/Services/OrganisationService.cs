972e7106e0d150765ea8b02448b00d57f936c5cb
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-08 11:34:53 +0100

    Implement create organisation method

--- HNG_Organisation/Services/OrganisationService.cs
+++ HNG_Organisation/Services/OrganisationService.cs
@@ -1,13 +1,32 @@
-﻿using System.Runtime.InteropServices;
+﻿using HNG_Organisation.Data;
+using HNG_Organisation.Entities;
+using HNG_Organisation.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
 
 namespace HNG_Organisation.Services;
 
 public class OrganisationService
 {
-    // Create org
-    public async Task CreateOrganisationAsync()
-    {
+    private readonly ApplicationDbContext _context;
 
+    public OrganisationService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    // Create org
+    public async Task<Organisation> CreateOrganisationAsync(OrganisationModel model)
+    {
+        var organisation = new Organisation()
+        {
+            Name = model.Name,
+            Description = model.Description
+        };
+
+        await _context.Organisations.AddAsync(organisation);
+        await _context.SaveChangesAsync();
+
+        return organisation;
     }
 
     // Get organisation
