15875fdd66c5bbaecbf2e242f0b731795eaf7803
Ibrahim Suleiman <ebeeraheem@gmail.com>
2024-07-08 19:51:46 +0100

    Add user dto

---
+++ HNG_Organisation/Models/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HNG_Organisation.Models;
+
+public class UserDto
+{
+    public string UserId { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
